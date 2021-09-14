782cdf31549c0f7313c8ea9a882541db5fcbb2bf
Usernow <usernow.82@gmail.com>
2021-09-14 10:23:17 -0500

    Estructura Projecto

---
+++ src/test/java/com/techstore/storeApp/StoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstore.storeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
