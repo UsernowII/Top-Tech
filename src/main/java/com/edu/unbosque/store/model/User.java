0d3f2cb7b49d837661abb9b039da37997ae74a75
victor <victortk1043@gmail.com>
2021-10-08 21:01:31 -0500

    commit prueba
    estoy probando otra formoa de comiittear

--- src/main/java/com/edu/unbosque/store/model/User.java
+++ src/main/java/com/edu/unbosque/store/model/User.java
@@ -30,5 +30,58 @@ public class User {
     @Getter @Setter @Column(name = "user")
     private String nick;
 
+	public User() {
+		super();
+	}
+
+	public User(Long id, String nameUser, String emailUser, String password, String nick) {
+		super();
+		this.id = id;
+		this.nameUser = nameUser;
+		this.emailUser = emailUser;
+		this.password = password;
+		this.nick = nick;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNameUser() {
+		return nameUser;
+	}
+
+	public void setNameUser(String nameUser) {
+		this.nameUser = nameUser;
+	}
+
+	public String getEmailUser() {
+		return emailUser;
+	}
+
+	public void setEmailUser(String emailUser) {
+		this.emailUser = emailUser;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
 
 }
